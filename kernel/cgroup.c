e43d9887546b908c074cbda02a4586074de27296
Pierre2324 <pdbbasketball@gmail.com>
2020-03-24 11:28:38 -0400

    cgroup: Boost some devfreq devices when a forked process gets to top-app
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>
    Signed-off-by: ihklknz <haikalluthfianinobalukia@proton.me>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -3005,9 +3005,10 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	/* This covers boosting for app launches and app transitions */
 	if (!ret && !threadgroup &&
 	    !strcmp(of->kn->parent->name, "top-app") &&
-	    task_is_zygote(tsk->parent->pid)) {
-		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+	    task_is_zygote(tsk->parent)) {
+		cpu_input_boost_kick_max(1000);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 1000);
 	}
 
 	put_task_struct(tsk);
